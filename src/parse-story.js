8332d20d41c386e6af9e67f02906551184013e92
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-04-07 23:58:44 -0400

    Iterate on card parsing functionality.

--- src/parse-story.js
+++ src/parse-story.js
@@ -1,16 +1,23 @@
 const parseDirectory = require('parse-dir')
 const frontMatter = require('front-matter')
+const path = require('path')
 
 function validate () {
 
 }
 
-parseDirectory('./cards/**/*.md', (err, files) => {
+parseDirectory(__dirname + '/cards/**/*.md', (err, files) => {
   if (err) {
     throw err
   }
 
+  console.log(JSON.stringify(files))
+
   const cards = files.map(file => frontMatter(file.contents))
+  cards.forEach(card => {
+    card.body = card.body.trim()
+  })
+
   validate(cards)
-  console.log(JSON.stringify(cards))
+  console.log(JSON.stringify(cards, undefined, 2))
 })
